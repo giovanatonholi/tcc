09830ac1f83655494469d7d34983f26a11196538
GIOVANA TONHOLI <00001109509418sp@aluno.educacao.sp.gov.br>
2024-11-06 15:39:45 -0300

    TCC COMPLETO

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function trocarFrase1() {
+    document.getElementById("frase1").textContent = "Atrás de toda mulher poderosa está ela mesma, lutando todos os dias para alcançar seus objetivos.";
+}
+
+function trocarFrase2() {
+    document.getElementById("frase2").textContent = "A beleza começa no momento em que você decide ser você mesma!";
+}
+
+function trocarFrase3() {
+    document.getElementById("frase3").textContent = "Você é a rainha da sua própria história, reivindique seu trono todos os dias.";
+}
+
+function trocarFrase4() {
+    document.getElementById("frase4").textContent = "Sua cor é sua coroa, use-a com orgulho e continue escrevendo sua própria narrativa!";
+}
